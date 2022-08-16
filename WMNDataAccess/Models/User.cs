cf374be3095b689410e32f56cd05971a53b0fa71
Mohammed Thalha <mohammed.t@ibridgellc.com>
2022-08-16 23:10:02 +0530

    Created new project for models

---
+++ WMNDataAccess/Models/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMNDataAccess.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string EmailID { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string? MiddleName { get; set; }
+
+        [MaxLength(50)]
+        public string? LastName { get; set; }
+
+        public Gender? Gender { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+        public int? PhoneNumber { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Others
+    }
+}
