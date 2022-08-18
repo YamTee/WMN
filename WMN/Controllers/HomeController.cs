44976796986c25f6cb7089e643ee7ca3b5e2d2cf
Mohammed Thalha <mohammed.t@ibridgellc.com>
2022-08-18 20:07:19 +0530

    Added Registration page

--- WMN/Controllers/HomeController.cs
+++ WMN/Controllers/HomeController.cs
@@ -1,21 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WMN.Models;
+using WMNDataAccess.Models;
+using WMNDataAccess.DataAccess;
 
 namespace WMN.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDBContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDBContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [Route("register-user")]
+        public IActionResult CreateUser()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        [Route("register-user")]
+        public IActionResult CreateUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Users.Add(user);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
         }
 
         public IActionResult Privacy()
